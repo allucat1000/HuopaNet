91e72cf09f8b4d955cd0e824507084103ee9f7ad
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-02-26 18:39:23 +0200

    yay

--- server/dns.ts
+++ server/dns.ts
@@ -6,17 +6,30 @@ import { HuopaNetPacket, VERSION, HuopaNetUrlObject } from "./core.ts";
 
 interface DNSStoreItem extends HuopaNetUrlObject {
     auth: string;
+    ip: string;
 }
 
-let DNSStore: Record<string, DNSStoreItem> = { "example.com": { auth: "a", "domain":"localhost", "port":8000, path: [], protocol: "http" }};
+interface IPStoreItem {
+    ip: string;
+    lastRegister: number;
+}
+
+const kv = await Deno.openKv();
+
+let DNSStore: Record<string, DNSStoreItem> = {};
+let registrationIps: Array<IPStoreItem> = [];
 
 try {
-    const t = await Deno.readFile("dnsstore");
-    if (t)
-        DNSStore = decode(t) as Record<string, DNSStoreItem>;
+    const t = await kv.get<Uint8Array>(["dnsstore"]);
+    const ips = await kv.get<Uint8Array>(["registrationips"])
+    if (t?.value)
+        DNSStore = decode(t.value) as Record<string, DNSStoreItem>;
+    if (ips?.value)
+        registrationIps = decode(ips.value) as Array<IPStoreItem>;
         
 } catch {
-    Deno.writeFile("dnsstore", encode(DNSStore));
+    await kv.set(["dnsstore"], encode(DNSStore));
+    await kv.set(["registrationips"], encode(registrationIps));
 }
 
 interface HuopaNetDNSResolveRequest extends HuopaNetPacket, HuopaNetUrlObject {
@@ -63,7 +76,9 @@ export class HuopaNetDNSServer {
             }
         });
 
-        this.wss.on("connection", (s: WebSocket) => {
+        // deno-lint-ignore no-explicit-any
+        this.wss.on("connection", (s: WebSocket, req: any) => {
+            const ip = req.socket.remoteAddress;
             console.log("Client connected");
             s.onmessage = async (r: MessageEvent) => {
                 let bytes: Uint8Array;
@@ -80,13 +95,13 @@ export class HuopaNetDNSServer {
         
                 const d = decode(bytes);
                 if (typeof d !== "object" || d === null) {
-                    return s.send(encode(encode({
+                    return s.send(encode({
                         cmd: "dns_error",
                         ok: false,
                         error: "BAD_REQUEST",
                         code: 0,
                         body: `Invalid format`
-                    })));
+                    }));
                 }
                 const p = d as HuopaNetPacket;
                 switch (p.cmd) {
@@ -106,7 +121,7 @@ export class HuopaNetDNSServer {
                             const da = DNSStore[url];
                             
                             if (da) {
-                                const { auth: _, ...re } = da;
+                                const { auth: _, ip: __, ...re } = da;
                                 const res: HuopaNetUrlObject = re;
                                 return s.send(encode({
                                     cmd: "dns_resolve",
@@ -152,8 +167,70 @@ export class HuopaNetDNSServer {
                                 body: `Unsupported version ${o.version}`
                             }));
                         
+                        if (!o || !o.key || !o.cmd || !o.secure || !o.version || !o.http || !o.hnwp || !o.http.domain || !o.http.path || !o.hnwp.domain || !o.hnwp.path)
+                            return s.send(encode({
+                                cmd: "dns_error",
+                                ok: false,
+                                error: "BAD_REQUEST",
+                                code: 0,
+                                body: `Invalid format`
+                            }));
+                        
+                        if (o.key.length < 32)
+                            return s.send(encode({
+                                cmd: "dns_error",
+                                ok: false,
+                                error: "BAD_REQUEST",
+                                code: 0,
+                                body: `Expected 32 or more characters for the DNS registration auth key`
+                            }));
+                            
+                        const url: string = `${o.hnwp.subdomain ? `${o.hnwp.subdomain}.` : ""}${o.hnwp.domain}`;
+                        const domain: string = o.hnwp.domain;
+                        const da = DNSStore[url];
+                        if (da) {
+                            if (da.auth !== o.key) 
+                                return s.send(encode({
+                                    cmd: "dns_register",
+                                    ok: false,
+                                    error: "DOMAIN_ALREADY_REGISTERED",
+                                    code: 2,
+                                    body: `Domain ${url} already registered under different key`
+                                }));
+                        }
 
-                        break;
+                        const last = registrationIps.find(r => r.ip == ip);
+                        if (last?.lastRegister && last.lastRegister > Date.now() - 86400000) {
+                            return s.send(encode({
+                                cmd: "dns_register",
+                                ok: false,
+                                error: "RATE_LIMIT",
+                                code: 5,
+                                body: `You have already registered a domain in the previous 24 hours, please wait ${Math.round(((Date.now() - 86400000) - last.lastRegister) / 360000)} more hours`
+                            }));
+                        }
+
+                        if (o.hnwp.subdomain) {
+                            const mainDomainData = DNSStore[domain];
+                            if (mainDomainData.auth !== o.key)
+                                return s.send(encode({
+                                    cmd: "dns_register",
+                                    ok: false,
+                                    error: "NOT_IMPLEMENTED",
+                                    code: 4,
+                                    body: `Too lazy to implement this yet, please don't register subdomains with a different key than the main domain's key`
+                                }));
+                        }
+
+                        DNSStore[url] = { ...o.http, auth: o.key, ip };
+
+                        console.log(`[${ip}] Registered domain ${url}, HTTP domain ${JSON.stringify(o.http)}`);
+
+                        return s.send(encode({
+                            cmd: "dns_register",
+                            ok: true,
+                            body: `Registered domain sucesssfully`
+                        }));
                     }
                     
                     default: {
