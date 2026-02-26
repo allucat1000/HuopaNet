8e71cf67f79deb4d9203872fda80150263e72484
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2026-02-26 21:36:28 +0100

    debuggin

--- server/dns.ts
+++ server/dns.ts
@@ -60,6 +60,7 @@ export class HuopaNetDNSServer {
             };
 
             s.onmessage = async(r: MessageEvent)=> {
+                console.log("message")
                 let bytes: Uint8Array;
         
                 if (r.data instanceof ArrayBuffer) {
